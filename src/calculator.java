0a83611959facdc3c5f419f4477b90a0f938c294
Siddhartha Srivastava <siddharthasr@umass.edu>
2021-08-18 23:43:06 +0000

    edited the code a little bit

--- src/calculator.java
+++ src/calculator.java
@@ -58,7 +58,6 @@ public class calculator{
 	        }
 	        catch (NumberFormatException ex){
 	        	System.out.println("Invalid input. Try Again");
-	        	found = false;
 	        	break;
 	        }
 			}
